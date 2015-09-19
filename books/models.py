2f943546f0bd738f67c512ce029533aadac4d017
“Amol <amol_thite@tekdi.net>
2015-09-19 18:55:45 +0530

    admin model changes

--- books/models.py
+++ books/models.py
@@ -16,7 +16,9 @@ class Publisher(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
-    email = models.EmailField()
+    email = models.EmailField(verbose_name='e-mail')
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
